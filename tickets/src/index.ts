80248a626857fbebe738185b6c4c249a62a3398d
Dave Briley <davebriley68@gmail.com>
2023-10-12 19:44:30 -0400

    Test load

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -44,7 +44,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Tickets service is listening on port 3000');
+    console.log('Tickets service is listening on port:3000');
   });
 };
 
