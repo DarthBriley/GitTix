dd3e293b64a1d1181a117d193f829fbed0e5cec0
Dave Briley <davebriley68@gmail.com>
2023-10-12 20:17:55 -0400

    Test load 5

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-create-listener';
 
 const start = async () => {
-  console.log('Starting up payments service ...');
+  console.log('Starting up payments-service ...');
   if (!process.env.JWT_KEY) {
     throw new Error('Must have JWT_KEY defined');
   }
@@ -44,7 +44,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Payments service is listening on port 3000');
+    console.log('Payments service is listening on port:3000');
   });
 };
 
